ec3251ba7853acd7430562545271d02a16d383fc
Олег Чеботарев <oleg4bat@gmail.com>
2023-04-10 16:05:13 +0300

    fix

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
